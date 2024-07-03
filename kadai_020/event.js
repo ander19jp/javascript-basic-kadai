a05f1d9ec41ae1d50947dce8ffe6830dd57746bc
ander19jp <ander19jp@gmail.com>
2024-07-03 20:22:35 +0900

    課題提出

---
+++ kadai_020/event.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const button = document.getElementById('btn');
+const text = document.getElementById('text');
+
+ button.addEventListener('click',()=>{
+    text.textContent = 'ボタンをクリックしました';
+
+ });
